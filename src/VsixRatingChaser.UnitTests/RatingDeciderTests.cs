ded0f0b3d19bb8407caa6cbd44f0f018c8af9017
Greg Trevellick <gtrevellick@googlemail.com>
2017-12-30 19:00:47 +0000

    .

---
+++ src/VsixRatingChaser.UnitTests/RatingDeciderTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VsixRatingChaser.UnitTests
+{
+    [TestClass]
+    public class RatingDeciderTests
+    {
+        [DataTestMethod]
+        [DataRow(0, 0, false)]
+        [DataRow(0, 1, false)]
+        [DataRow(1, 0, true)]
+        [DataRow(1, 1, false)]
+        [DataRow(-1, 1, false)]
+        public void ExceededRatingRequestLimitTest(int ratingRequestCount, int ratingRequestLimit, bool expected)
+        {
+            // Arrange
+            var sut = new RatingDecider();
+
+            // Act
+            var actual = sut.ExceededRatingRequestLimit(ratingRequestCount, ratingRequestLimit);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- src/VsixRatingChaser/RatingDecider.cs
+++ src/VsixRatingChaser/RatingDecider.cs
@@ -24,12 +24,12 @@ namespace VsixRatingChaser
             return shouldShowDialog;
         }
 
-        private bool ExceededRatingRequestLimit(int ratingRequestCount, int ratingRequestLimit)//gregt unit test reqd
+        internal bool ExceededRatingRequestLimit(int ratingRequestCount, int ratingRequestLimit)
         {
             return ratingRequestCount > ratingRequestLimit;
         }
 
-        private bool ExceededRatingRequestGap(DateTime lastRatingRequest, int ratingRequestGap)//gregt unit test reqd
+        internal bool ExceededRatingRequestGap(DateTime lastRatingRequest, int ratingRequestGap)//gregt unit test reqd
         {
             var now = DateTime.Now;
             var acceptableDate = now.AddSeconds(-1 * ratingRequestGap);//gregt change AddSeconds to AddMonths
